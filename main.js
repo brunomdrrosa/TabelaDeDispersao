464529cd6de695d612f0bd77374efc02890cc6b9
Bruno Machado <brunomdr46@gmail.com>
2023-08-30 09:00:38 -0300

    Cenário do conto pequeno finalizado

--- main.js
+++ main.js
@@ -1,49 +1,92 @@
-const fs = require('fs')
+const fs = require("fs");
+const readline = require("readline");
 
-fs.readFile('tinytale.txt', 'utf8', (err, data) => {
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function menu() {
+  console.log("=== MENU ===");
+  console.log("Você deseja ver a tabela de dispersão de quais opções?");
+  console.log("1. Conto pequeno");
+  console.log("2. Conto grande (Um conto de duas cidades - Charles Dickens)");
+  rl.question("Digite o número da opção desejada: ", function (answer) {
+    if (answer === "1") {
+      programa("tinytale.txt", 293);
+      rl.close();
+    } else if (answer === "2") {
+      programa("tale.txt", 19597);
+      rl.close();
+    } else {
+      console.log("Digite um número de 1 a 2\n");
+      menu();
+    }
+  });
+}
+
+function programa(nomeArquivo, tamanhoTabela) {
+  fs.readFile(nomeArquivo, "utf8", (err, data) => {
     var listaFrases = data.toString().split(/\s+/);
 
     var contadorDePalavras = {};
 
     for (const palavra of listaFrases) {
-        if (contadorDePalavras[palavra]) {
-            contadorDePalavras[palavra]++;
-        } else {
-            contadorDePalavras[palavra] = 1;
-        }
+      if (contadorDePalavras[palavra]) {
+        contadorDePalavras[palavra]++;
+      } else {
+        contadorDePalavras[palavra] = 1;
+      }
     }
 
-    var palavrasEmOrdem = Object.keys(contadorDePalavras).sort((a, b) => contadorDePalavras[b] - contadorDePalavras[a]);
+    var palavrasEmOrdem = Object.keys(contadorDePalavras).sort(
+      (a, b) => contadorDePalavras[b] - contadorDePalavras[a]
+    );
 
-    var listaTabelaAscii = []
+    var listaTabelaAscii = [];
 
     for (let index = 0; index < palavrasEmOrdem.length; index++) {
-        const totalSum = palavrasEmOrdem[index].split("").reduce((sum, element) => sum + element.charCodeAt(0), 0);
-        listaTabelaAscii.push(totalSum)
+      const totalSum = palavrasEmOrdem[index]
+        .split("")
+        .reduce((sum, element) => sum + element.charCodeAt(0), 0);
+      listaTabelaAscii.push(totalSum);
     }
 
-    var listaResto = []
+    var listaResto = [];
 
-    listaTabelaAscii.forEach(element => {
-        listaResto.push(hash(element, 293))
+    listaTabelaAscii.forEach((element) => {
+      listaResto.push(hash(element, tamanhoTabela));
     });
 
-    console.log(listaResto)
+    console.log(listaResto);
 
-    var arrayFinal = Array(293)
+    var arrayFinal = Array(tamanhoTabela);
 
     for (let index = 0; index < listaResto.length; index++) {
-        palavra = palavrasEmOrdem[index]
-        console.log(palavra)
-        quantidade = contadorDePalavras[palavra]
-        const objeto = { posicao: listaResto[index], palavra: palavra, quantidade: quantidade };
-        arrayFinal[listaResto[index]] = objeto
+      const palavra = palavrasEmOrdem[index];
+      const quantidade = contadorDePalavras[palavra];
+      let posicao = listaResto[index];
+
+      while (arrayFinal[posicao]) {
+        posicao = (posicao + 1) % arrayFinal.length;
+      }
+
+      const objeto = {
+        posicao: posicao,
+        palavra: palavra,
+        quantidade: quantidade,
+      };
+
+      arrayFinal[posicao] = objeto;
+    console.log(palavra);
+
     }
-    console.log(arrayFinal)
-
-
-});
+    console.log(arrayFinal);
+  });
+}
 
 function hash(value, M) {
-    return value % M;
+  return value % M;
 }
+
+menu();
